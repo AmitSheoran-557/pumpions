7d7b098eb086f8870a8115a41ed151e3996b4255
AmitSheoran-557 <amit22082006@gmail.com>
2025-03-31 13:00:16 +0530

    update code

--- src/components/MembershipPerks.jsx
+++ src/components/MembershipPerks.jsx
@@ -11,7 +11,7 @@ import "swiper/css/pagination";
 
 const MembershipPerks = () => {
     return (
-        <div className="flex justify-center items-center w-full bg-dark-green-2 -mt-1 relative">
+        <div className="flex justify-center membership-perks items-center w-full bg-dark-green-2 -mt-1 relative">
             <div className="relative pb-[102px] max-lg:pb-[127px] max-md:pb-[50px] w-full">
                 <div className="max-lg:max-w-[770px] mx-auto pt-[97px] max-lg:pt-[70px] max-md:pt-[34px] px-5">
                     <p className="text-xl pb-5 text-white uppercase text-center max-md:text-base max-md:pb-3"> Community + Funding + Sunset</p>
@@ -23,18 +23,46 @@ const MembershipPerks = () => {
                             <Image className="xl:max-w-[870px] lg:max-w-[670px] lg:pe-7 xl:pe-0 pointer-events-none max-sm:px-1 w-full" src="/assets/images/webp/clubhouse-img.webp" width={870} height={446} alt="club-house-img" />
                         </div>
                         <div className="lg:w-4/12 w-full">
-                            <div className="flex w-full flex-wrap xl:gap-2 gap-1.5">
-                                {MEMBERSHIP_PERKS_DATA_LIST.map((obj, i) => (
-                                    <div key={i} className="w-full xl:max-w-[181px] lg:max-w-[150px] xl:px-4 px-3 max-w-[278px] hover:bg-[#E8770E] bg-white rounded-lg flex flex-col items-center justify-center xl:py-[26px] py-5 bg-[url(/assets/images/webp/membership-cards-border.webp)] duration-300 ease-linear bg-[length:100%_100%] bg-no-repeat" >
-                                        <Image width={48} height={49} className="max-w-12 lg:mb-4 mb-3" src={obj.image} alt={obj.title} />
-                                        <p className="text-center lg:text-xs md:text-base">
-                                            {obj.title}
-                                        </p>
-                                        <p className="opacity-70 lg:text-[6px] text-center pt-1 max-xl:text-[10px]">
-                                            {obj.description}
-                                        </p>
-                                    </div>
-                                ))}
+                            <div className="w-full lg:block max-lg:hidden">
+                                <div className="flex w-full flex-wrap xl:gap-2 gap-1.5">
+                                    {MEMBERSHIP_PERKS_DATA_LIST.map((obj, i) => (
+                                        <div key={i} className="w-full xl:max-w-[181px] lg:max-w-[150px] xl:px-4 px-3 max-w-[278px] hover:bg-[#E8770E] bg-white rounded-lg flex flex-col items-center justify-center xl:py-[26px] py-5 bg-[url(/assets/images/webp/membership-cards-border.webp)] duration-300 ease-linear bg-[length:100%_100%] bg-no-repeat" >
+                                            <Image width={48} height={49} className="max-w-12 lg:mb-4 mb-3" src={obj.image} alt={obj.title} />
+                                            <p className="text-center lg:text-xs md:text-base">
+                                                {obj.title}
+                                            </p>
+                                            <p className="opacity-70 lg:text-[6px] text-center pt-1 max-xl:text-[10px]">
+                                                {obj.description}
+                                            </p>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+
+                            <div className="w-full lg:hidden max-lg:block">
+                                <Swiper
+                                    spaceBetween={16}
+                                    slidesPerView={1.2}
+                                    pagination={{ clickable: true }}
+                                    modules={[Pagination]}
+                                    loop={true}
+                                    className="mySwiper"
+                                >
+
+                                    {MEMBERSHIP_PERKS_DATA_LIST.map((obj, index) => (
+                                        <SwiperSlide key={index} className="flex flex-col items-center">
+                                            <div key={index} className="w-full xl:max-w-[181px] lg:h-[200px] h-[307px] lg:max-w-[150px] xl:px-4 lg:px-3 md:px-8 px-9 max-w-[278px] hover:bg-[#E8770E] bg-white rounded-lg flex flex-col items-center justify-center xl:py-[26px] py-5 bg-[url(/assets/images/webp/membership-cards-border.webp)] duration-300 ease-linear bg-[length:100%_100%] bg-no-repeat" >
+                                                <Image width={48} height={49} className="max-w-12 lg:mb-4 mb-3" src={obj.image} alt={obj.title} />
+                                                <p className="text-center uppercase !leading-[204%] lg:text-xs md:text-base">
+                                                    {obj.title}
+                                                </p>
+                                                <p className="opacity-70 uppercase lg:text-[6px] !leading-[204%] text-center pt-1 max-xl:text-[10px]">
+                                                    {obj.description}
+                                                </p>
+                                            </div>
+                                        </SwiperSlide>
+                                    ))}
+                                </Swiper>
                             </div>
                         </div>
                     </div>
--- src/components/OgPumpions.jsx
+++ src/components/OgPumpions.jsx
@@ -13,12 +13,12 @@ import 'swiper/css/pagination';
 
 const OgPumpions = () => {
     return (
-        <div id='team' className='w-full flex justify-center bg-dark-green-2 items-center -mt-1'>
+        <div id='team' className='w-full flex justify-center bg-dark-green-2 items-center -mt-1 og-pumpions'>
             <div className="max-[1660px]:max-w-[1920px] xl:max-w-[1204px] lg:max-w-[992px] md:max-w-[768px] max-w-[375px] mx-auto px-8 w-full">
                 <CustomHeading customClass="lg:mb-[79px] md:mb-[75px] mb-[30px]" title="OG PUMPIONS" />
 
-                <div className="w-full lg:block max-lg:hidden">
-                    <div className="flex flex-wrap w-full mx-auto justify-between lg:mb-[61px] md:mb-[53px] mb-[30px]">
+                <div className="w-full md:block max-md:hidden">
+                    <div className="flex flex-wrap w-full mx-auto lg:justify-between justify-center lg:gap-0 md:gap-x-36 md:gap-y-[45px] lg:mb-[61px] md:mb-[53px] mb-[30px]">
                         {OG_PUMPIONS_DATA_LIST.map((item, index) => (
                             <div className="max-w-[226px] w-full" key={index}>
                                 <Image className="lg:max-w-[226px] w-full mb-[31px]" src={item.image} width={226} height={208} alt="card-img" />
@@ -37,18 +37,18 @@ const OgPumpions = () => {
                     </div>
                 </div>
 
-                <div className="w-full lg:hidden max-lg:block">
+                <div className="w-full md:hidden max-md:block md:pb-[53px] pb-[29px]">
                     <Swiper
                         spaceBetween={16}
                         slidesPerView={1.2}
                         pagination={{ clickable: true }}
-                        navigation
-                        modules={[Pagination, Navigation]}
+                        modules={[Pagination]}
+                        loop={true}
                         className="mySwiper"
                     >
                         {OG_PUMPIONS_DATA_LIST.map((item, index) => (
-                            <SwiperSlide key={index} className="flex flex-col items-center">
-                                <Image className="w-full mb-[15px]" src={item.image} width={226} height={208} alt="card-img" />
+                            <SwiperSlide key={index} className="flex flex-col max-w-max items-center">
+                                <Image className="w-full mb-[15px] max-w-[226px]" src={item.image} width={226} height={208} alt="card-img" />
                                 <h4 className='text-white text-center text-xl leading-snug'>{item.name}</h4>
                                 <p className='text-white/70 text-center text-sm mb-4'>{item.role}</p>
                                 <div className="flex gap-3 justify-center">
